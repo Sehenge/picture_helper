6a6bd6edc9dc62600f4dbb2ea569da38b1ad9d41
Sehenge <sehenge@gmail.com>
2013-03-19 16:59:57 +0300

    Getting Dir + Parsing + Frontend

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,17 +7,28 @@ $this->pageTitle=Yii::app()->name;
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
     <form class="input" action="?r=site/getdir" method="post">
-        <input type="text" placeholder="ASIN" name="asin"/>
+        <div class="select">
+            <select name="brand">
+                <?php
+                foreach($brands as $brand) {
+                    echo '<option>', $brand, '</option>';
+                }
+                ?>
+            </select>
+        </div>
+        <input type="text" placeholder="SKU" name="sku"/>
+        <input type="text" placeholder="Model" name="model"/>
+        <input type="text" placeholder="Color Code" name="color_code"/>
         <?php
-        echo CHtml::ajaxSubmitButton('Submit', CHtml::normalizeUrl(array('site/getdir')),
+        echo CHtml::ajaxSubmitButton('Search', CHtml::normalizeUrl(array('site/getdir')),
             array(
                 'data'=>'js:jQuery(this).parents("form").serialize()+"&isAjaxRequest=1"',
                 'success'=>
                 'function(data){
-                      $("#searchResult").html(data);
+                      helper = new Helper();
+                      helper.PrintImages(data);
                       return false;
                  }'
-
             ),
             array(
                 'id'=>'ajaxSubmit',
@@ -28,4 +39,13 @@ $this->pageTitle=Yii::app()->name;
 
 <div class="output">
     <textarea readonly id="searchResult"></textarea>
+</div>
+
+<div class="img_container">
+    <div class="eyewear"></div>
+    <div class="cases">
+        <form>
+
+        </form>
+    </div>
 </div>
